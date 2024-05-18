17b62ace4793afaf5f0848b6c6469cd05c1d4455
saranrajmcse <msraj1584>
2024-05-18 18:35:10 +0530

    Add video quality selection feature

--- netlify/functions/download.js
+++ netlify/functions/download.js
@@ -16,7 +16,7 @@ exports.handler = async function(event, context) {
 
         const availableQualities = formats.map(format => ({
             quality: format.qualityLabel,
-            itag: format.itag
+            format: `${format.container.toUpperCase()} - ${format.resolution} - ${format.encoding}`
         }));
 
         return {
